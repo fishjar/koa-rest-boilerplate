85ba546794f41091d0d367ec70c8b0a808bcf37e
Gabe Yuan <yugang2002@gmail.com>
2019-09-12 16:09:07 +0800

    add replace files

--- swagger/replace/initData.js
+++ swagger/replace/initData.js
@@ -26,9 +26,9 @@ export default async () => {
     });
     const guestRole = await model.Role.create({ name: "guest" });
 
-    // 创建团队
-    const titanicGroup = await model.Group.create({ name: "titanic" });
-    const rayjarGroup = await model.Group.create({ name: "rayjar" });
+    // // 创建团队
+    // const titanicGroup = await model.Group.create({ name: "titanic" });
+    // const rayjarGroup = await model.Group.create({ name: "rayjar" });
 
     // 关联用户角色
     await user.addRole(userRole);
@@ -38,37 +38,37 @@ export default async () => {
     await jack.addRole(guestRole);
     await rose.addRole(guestRole);
 
-    // 关联用户团队
-    await user.addGroup(rayjarGroup, {
-      through: { level: 10, joinTime: new Date() },
-    });
-    await jack.addGroup(rayjarGroup, {
-      through: { level: 2, joinTime: new Date() },
-    });
-    await jack.addGroup(titanicGroup, {
-      through: { level: 2, joinTime: new Date() },
-    });
-    await rose.addGroup(titanicGroup, {
-      through: { level: 3, joinTime: new Date() },
-    });
+    // // 关联用户团队
+    // await user.addGroup(rayjarGroup, {
+    //   through: { level: 10, joinTime: new Date() },
+    // });
+    // await jack.addGroup(rayjarGroup, {
+    //   through: { level: 2, joinTime: new Date() },
+    // });
+    // await jack.addGroup(titanicGroup, {
+    //   through: { level: 2, joinTime: new Date() },
+    // });
+    // await rose.addGroup(titanicGroup, {
+    //   through: { level: 3, joinTime: new Date() },
+    // });
 
-    // 关联用户友谊
-    await user.addFriend(jack);
-    await user.addFriend(rose);
-    await jack.addFriend(rose);
+    // // 关联用户友谊
+    // await user.addFriend(jack);
+    // await user.addFriend(rose);
+    // await jack.addFriend(rose);
 
-    // 创建测试鉴权
-    await model.Auth.create({
-      userId: jack.id,
-      authType: "account",
-      authName: jack.name,
-      authCode: sign.signPwd(jack.name, DEFAULT_PASSWORD),
-      verifyTime: new Date(),
-      expireTime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-    });
-    console.log("\n测试帐号");
-    console.log(`username: ${jack.name}`);
-    console.log(`password: ${DEFAULT_PASSWORD}\n`);
+    // // 创建测试鉴权
+    // await model.Auth.create({
+    //   userId: jack.id,
+    //   authType: "account",
+    //   authName: jack.name,
+    //   authCode: sign.signPwd(jack.name, DEFAULT_PASSWORD),
+    //   verifyTime: new Date(),
+    //   expireTime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+    // });
+    // console.log("\n测试帐号");
+    // console.log(`username: ${jack.name}`);
+    // console.log(`password: ${DEFAULT_PASSWORD}\n`);
 
     // 创建默认鉴权
     await model.Auth.create({
